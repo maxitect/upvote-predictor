eb1b5b8be2f1dc1bee0fbd123d2d85efdcc3b4c4
maxitect <maxime.downe@gmail.com>
2025-04-18 13:25:24 +0100

    refactor: update 02_train_hn_regressor.py with new config

--- 02_train_hn_regressor.py
+++ 02_train_hn_regressor.py
@@ -7,6 +7,8 @@ import psycopg
 from tqdm import tqdm
 import model
 import dataset
+import config
+import os
 
 dev = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 ts = datetime.datetime.now().strftime('%Y_%m_%d__%H_%M_%S')
@@ -17,38 +19,42 @@ query = "SELECT title, domain, timestamp, score FROM stories"
 df = pd.read_sql(query, conn)
 conn.close()
 
-vocab_to_int = pickle.load(open('./tkn_words_to_ids.pkl', 'rb'))
-int_to_vocab = pickle.load(open('./tkn_ids_to_words.pkl', 'rb'))
+vocab_to_int = pickle.load(open(config.VOCAB_TO_ID_PATH, 'rb'))
 
 ds = dataset.HackerNewsDataset(
     df['title'].values,
     df['domain'].values,
     df['timestamp'].values,
     df['score'].values,
-    vocab_to_int
+    vocab_to_int,
+    max_title_len=config.MAX_TITLE_LENGTH
 )
 
 train_size = int(0.8 * len(ds))
 val_size = len(ds) - train_size
 train_ds, val_ds = torch.utils.data.random_split(ds, [train_size, val_size])
 
-train_dl = torch.utils.data.DataLoader(train_ds, batch_size=64, shuffle=True)
-val_dl = torch.utils.data.DataLoader(val_ds, batch_size=64)
+train_dl = torch.utils.data.DataLoader(
+    train_ds, batch_size=config.REGRESSOR_BATCH_SIZE, shuffle=True)
+val_dl = torch.utils.data.DataLoader(
+    val_ds, batch_size=config.REGRESSOR_BATCH_SIZE)
 
-skipgram = model.SkipGram(len(vocab_to_int), 128)
-skipgram.load_state_dict(torch.load(
-    './checkpoints/2025_04_17__11_04_09.5.skipgram.pth'))
+skipgram = model.SkipGram(len(vocab_to_int), config.EMBEDDING_DIM)
+skipgram.load_state_dict(torch.load(config.SKIPGRAM_BEST_MODEL_PATH))
 skipgram.to(dev)
 skipgram.eval()
 
-regressor = model.Regressor(emb_dim=128, domain_size=len(ds.domain_to_idx))
+regressor = model.Regressor(
+    emb_dim=config.EMBEDDING_DIM, domain_size=len(ds.domain_to_idx))
 regressor.to(dev)
-optimizer = torch.optim.Adam(regressor.parameters(), lr=0.001)
+optimizer = torch.optim.Adam(regressor.parameters(), lr=config.REGRESSOR_LR)
 criterion = torch.nn.MSELoss()
 
 wandb.init(project='mlx7-week1-regressor', name=f'{ts}')
 
-for epoch in range(10):
+best_val_loss = float('inf')
+
+for epoch in range(config.REGRESSOR_EPOCHS):
     regressor.train()
     train_loss = 0
     train_progress = tqdm(
@@ -104,16 +110,19 @@ for epoch in range(10):
         f'Val Loss = {val_loss:.4f}'
     )
 
-    if (epoch + 1) % 5 == 0:
-        checkpoint_name = f'{ts}.{epoch + 1}.regressor.pth'
-        torch.save(regressor.state_dict(), f'./checkpoints/{checkpoint_name}')
-        artifact = wandb.Artifact('regressor-weights', type='model')
-        artifact.add_file(f'./checkpoints/{checkpoint_name}')
-        wandb.log_artifact(artifact)
+    checkpoint_name = f'{epoch + 1}.pth'
+    checkpoint_path = os.path.join(
+        config.REGRESSOR_CHECKPOINT_DIR, checkpoint_name)
+    torch.save(regressor.state_dict(), checkpoint_path)
 
-final_checkpoint = f'{ts}.final.regressor.pth'
-torch.save(regressor.state_dict(), f'./checkpoints/{final_checkpoint}')
-torch.save(ds.domain_to_idx, './checkpoints/domain_to_idx.pth')
+    if val_loss < best_val_loss:
+        best_val_loss = val_loss
+        torch.save(regressor.state_dict(), config.REGRESSOR_BEST_MODEL_PATH)
+        torch.save(ds.domain_to_idx, config.DOMAIN_MAPPING_PATH)
+        print(
+            f'New best model saved at epoch {epoch+1} '
+            f'with validation loss {best_val_loss:.4f}'
+        )
 
-print('Training completed!')
+print(f'Training completed! Best validation loss: {best_val_loss:.4f}')
 wandb.finish()
